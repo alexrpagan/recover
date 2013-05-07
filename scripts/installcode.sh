be70c53c77af7f5d40c0b004565c3df6dece4cfc
Alex Pagan <alexrpagan@gmail.com>
2013-05-07 15:56:10 -0400

    fixes

--- scripts/installcode.sh
+++ scripts/installcode.sh
@@ -5,15 +5,16 @@ user="apagan"
 
 while read host; do
     ssh $user@$host " . .bash_profile \
-                      && mkdir -p code
+                      && mkdir -p code \
                       && cd code \
                       && rm -rf xfertest \
                       && git clone https://github.com/alexrpagan/xfertest.git \
+                      && cd xfertest \
                       && . ./scripts/init.sh \
-                      && ./scripts/killservers.sh
+                      && ./scripts/killservers.sh \
                       && cd xfertest/src/main \
-                      && go run main.go -h $host" &
-done
+                      && tmux -c 'go run main.go -h $host'" &
+done < servers
 
 wait
 echo "done installing code"
