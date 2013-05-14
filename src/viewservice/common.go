9fb763dc84d91a042869d8c64b8cf79a2dd2c147
Alex Pagan <alexrpagan@gmail.com>
2013-05-14 08:44:43 -0400

    expand deadpings again

--- src/viewservice/common.go
+++ src/viewservice/common.go
@@ -5,7 +5,7 @@ import "time"
 
 
 const PING_INTERVAL = time.Millisecond * 100
-const DEAD_PINGS = 10
+const DEAD_PINGS = 12
 const REPLICATION_LEVEL = 3
 const CRITICAL_MASS = 5
 const NUMBER_OF_SHARDS = 100
