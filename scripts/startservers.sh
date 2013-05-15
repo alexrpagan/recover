11a7671c100d07ae2f72cb151edb6bc6a09357e0
Alex Pagan <alexrpagan@gmail.com>
2013-05-15 16:20:41 -0400

    kill servers first

--- scripts/startservers.sh
+++ scripts/startservers.sh
@@ -3,11 +3,17 @@ set -e
 
 user="apagan"
 
+echo "killing servers"
+while read host; do
+    ssh $user@$host ". /home/$user/code/xfertest/scripts/killservers.sh" &
+done < servers
+wait
+
+echo "starting servers"
 i=0
 while read host; do
     echo $i
-    ssh $user@$host "/home/$user/code/xfertest/scripts/killservers.sh" &
-    ssh $user@$host "nohup /home/$user/code/xfertest/src/main/xfer -me $i -hosts /home/$user/code/xfertest/scripts/servers" &
+    ssh $user@$host "nohup /home/$user/code/xfertest/src/main/xfer -me $i -hosts /home/$user/code/xfertest/scripts/servers > out" &
     ((i++))
 done < servers
 
